1e9b544bba9f0ffb45bb0570b6bf7473d52e9c36
Lucas Nobrega <bbloveforever@hotmail.com>
2026-02-02 16:54:13 -0300

    fix: show delivery not found case not exist

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -53,9 +53,14 @@ class DeliveryLogsController {
       },
     })
 
+    if(!delivery){
+      return response.status(404).json({message: "delivery not found"})
+    }
+
     if(request.user?.role === "customer" && request.user.id !== delivery?.userId) {
       throw new AppError("The user can only view their own deliveries", 401)
     }
+
 
     return response.json(delivery)
   }
